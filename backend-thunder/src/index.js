20c942bcf2bc018cbbea93253d48158e20b8d91a
javierjinan22 <javier.jinan22@gmail.com>
2023-06-30 12:53:09 +0700

    create produk masih error

--- backend-thunder/src/index.js
+++ backend-thunder/src/index.js
@@ -4,20 +4,23 @@ const express = require('express');
 const userRoutes = require('./routes/user');
 const productRoutes = require('./routes/product');
 const middlewareLogRequest = require('./middleware/log');
+const FileUpload = require('express-fileupload');
 const upload = require('./middleware/multer');
 const app = express();
 
 
 app.use(middlewareLogRequest);
 app.use(express.json());
+app.use(FileUpload());
 
 app.use('/user', userRoutes);
-app.use('/product', productRoutes);
+app.use(productRoutes);
 
 app.use(express.static('public/image'));
-app.use('/', (req, res) => {
-    res.send("Selamat datang di BACKEND THUNDERCATS")
-})
+
+// app.use('/', (req, res) => {
+//     res.send("Selamat datang di BACKEND THUNDERCATS")
+// })
 
 app.post('/upload', upload.single('photo'), (req, res) => {
     res.json({
@@ -25,6 +28,6 @@ app.post('/upload', upload.single('photo'), (req, res) => {
     })
 })
 
-app.listen(3001, function () {
+app.listen(3001, ()=>{
     console.log("server berhasil running");
 })
--- backend-thunder/src/routes/product.js
+++ backend-thunder/src/routes/product.js
@@ -1,27 +1,22 @@
-import {
-    getProducts,
-    getProductById,
-    saveProduct,
-    updateProduct,
-    deleteProduct
-} from "../controller/product"
+
 
 const express = require('express');
 const router = express.Router();
+const productController = require('../controller/product');
 
 // READ - GET
-router.get('/products', getProducts);
+// router.get('/products', productController.getAllProduct);
 
-// READ - GET by ID
-router.get('/products/:id', getProductById);
+// // READ - GET by ID
+// router.get('/products/:id', getProductById);
 
 // CREATE - POST
-router.post('/products', saveProduct);
+router.post('/product', productController.saveProduct);
 
 // UPDATE - PATCH
-router.patch('/products/:id', updateProduct);
+// router.patch('/products/:id', updateProduct);
 
-// DELETE - DELETE
-router.delete('/products/:id', deleteProduct);
+// // DELETE - DELETE
+// router.delete('/products/:id', deleteProduct);
 
-export default router;
+module.exports = router;
