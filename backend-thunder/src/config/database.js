9304e6797f4b2c3ba8458ac57d064982c1c7c168
unknown <zkyxentertain@gmail.com>
2023-07-02 18:06:06 +0700

    resolve index

--- backend-thunder/src/config/database.js
+++ backend-thunder/src/config/database.js
@@ -11,11 +11,7 @@ const mysql = require('mysql2');
 
 const { Sequelize } = require('sequelize');
 
-<<<<<<< HEAD
 const dbPool = new Sequelize('thundercat', 'root', '', {
-=======
-const dbPool = new Sequelize('thundercat_db','root','',{
->>>>>>> 499e8ad57939229adf829bdba21e7bc2ea0f56ef
     host: 'localhost',
     dialect: "mysql"
 });
--- backend-thunder/src/index.js
+++ backend-thunder/src/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
-<<<<<<< HEAD
-=======
 
 const adminRoutes = require('./routes/admin');
->>>>>>> 499e8ad57939229adf829bdba21e7bc2ea0f56ef
 const userRoutes = require('./routes/user');
 const pesanRoutes = require('./routes/pesan');
 const productRoutes = require('./routes/product');
@@ -17,7 +14,6 @@ app.use(middlewareLogRequest);
 app.use(express.json());
 app.use(FileUpload());
 
-<<<<<<< HEAD
 // Pengaturan Header CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Mengizinkan semua domain (harap disesuaikan dengan kebutuhan Anda)
@@ -28,10 +24,8 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/user', userRoutes);
-=======
 app.use('/pesan', pesanRoutes);
 app.use(userRoutes);
->>>>>>> 499e8ad57939229adf829bdba21e7bc2ea0f56ef
 app.use(productRoutes);
 app.use(adminRoutes);
 
